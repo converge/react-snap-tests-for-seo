822bcb552ce3610471106b85bd1102191854981a
João Vanzuita <joaovanzuita@me.com>
2019-07-14 22:26:06 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,26 @@ function App() {
   return (
     <div className="App">
       <Helmet>
-        <title>testing 123</title>
+        <title>testing 1234</title>
+        <meta property="og:title" content="testing 22222" />
+        <meta
+          property="og:image"
+          content="https://heuristic-noyce-d93351.netlify.com/_next/static/images/ok-714bdb599828eddb4ccde6e9d4aaf197.jpg"
+        />
+
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:image"
+          content="https://heuristic-noyce-d93351.netlify.com/_next/static/images/ok-714bdb599828eddb4ccde6e9d4aaf197.jpg"
+        />
+        <meta name="twitter:title" content="TITLE OF POST OR PAGE" />
+        <meta
+          name="twitter:description"
+          content="DESCRIPTION OF PAGE CONTENT"
+        />
+        <meta name="twitter:site" content="@joao_o" />
+        <meta name="twitter:site:id" content="54300137" />
+        <meta name="twitter:creator" content="@joao_o" />
       </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
